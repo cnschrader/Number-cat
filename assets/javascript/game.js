47e486646cf17ea951ea2a71bd5de2ddac861402
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-03-10 16:39:06 -0500

    pseudocode and images

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Creat variables for the counters. Wins, Losses, etc. 
+var wins = 0;
+var losses = 0;
+
+
+
+// Make an array for the number values of the crystals (1 - 19).
+
+var crystalChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+// look up how to make an array work without having to type a bunch of numbers
+var numberToMatch = []
+
+// Make a loop for the value given. Maybe inside the loop create the image tags, attach appropriate classes and ids, then append to the already exisiting div that has an id (think jquery for this.) .attr .data .append var -variable name- = $("<img>").
+
+// Make a reset funciton
+
+// Make an event function for the on clicks.
+
+// Make a function that will randomly select both a new number for the images and a new number to be guessed.
+
+// if statements that determine whether you win or lose (include counters, and call functions for reset and new random numbers. check through console.log).
+
