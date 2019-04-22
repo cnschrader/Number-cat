fece6dfb879f36079bb4aa990dbbd2f6dd4d9e5b
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-04-21 21:26:05 -0500

    added readme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,17 +13,6 @@ var lossCounter = $("#lossCounter");
 var goalArea = $("#goalArea");
 
 
-// Make an array for the number values of the crystals (1 - 19).
-
-
-
-// look up how to make an array work without having to type a bunch of numbers
-
-
-
-// Make a loop for the value given. Maybe inside the loop create the image tags, attach appropriate classes and ids, then append to the already exisiting div that has an id (think jquery for this.) .attr .data .append var -variable name- = $("<img>").
-
-// var catNumberChoices;
 
 
 var catChoicesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
@@ -33,8 +22,7 @@ var catNumberChoices;
 
 
 function reset() {
-    // catNumberChoices = catChoicesArray[Math.floor(Math.random() * catChoicesArray.length)];
-    //  catNumberOne = Math.floor(Math.random() * 12) + 1; 
+   
     catNumberOne.data('myval', Math.floor(Math.random() * 12) + 1);
     catNumberTwo.data('myval', Math.floor(Math.random() * 12) + 1);
     catNumberThree.data('myval', Math.floor(Math.random() * 12) + 1);
@@ -42,7 +30,7 @@ function reset() {
     console.log(catNumberOne.data('myval'));
     realNumber();
     score = 0;
-
+    
 };
 
 function range(start, end) {
@@ -68,36 +56,36 @@ function realNumber() {
 var wins = 1;
 var losses = 1;
 $(".catClick").on("click", function () {
-
+    
     score += $(this).data("myval");
-
+    
     if (score === numberGoal) {
-
+        
         alert("Meow! You Won!");
         winCounter.text(wins);
         wins++
         reset();
         console.log(wins);
-
+        
     }
-
+    
     else if (score > numberGoal) {
         alert("you lose!");
         lossCounter.text(losses);
         losses++
         reset();
         console.log(losses);
-
-
+        
+        
     };
-
+    
     playerScoreArea.text(score);
     console.log(numberGoal);
-
+    
     goalArea.text(numberGoal);
-
-
-
+    
+    
+    
 });
 
 
@@ -106,41 +94,22 @@ $(".catClick").on("click", function () {
 
 
 
-// var numberGoal = N[Math.floor(Math.random() * N.length)];
 
-// var numberGoal;
-// var numberToMatchArray = [];
-// function matchNumber(num){
-//     for(var i = 0; i <= 120; i++){
-//       (numberToMatchArray.push(num));
-//       console.log(numberToMatchArray);
+
+// Make an array for the number values of the crystals (1 - 19).
 
 
 
-//     }
-
-//   
-// 
+// look up how to make an array work without having to type a bunch of numbers
 
 
 
+// Make a loop for the value given. Maybe inside the loop create the image tags, attach appropriate classes and ids, then append to the already exisiting div that has an id (think jquery for this.) .attr .data .append var -variable name- = $("<img>").
+
+// var catNumberChoices;
 
 
 
-//   function matchNumber(num){
-//     for(var i = 0; i <= 120; i++){
-//         console.log(numberToMatchArray.push(num));
-//     }
-
-// };
-
-// 
-
-
-
-
-
-// });
 
 // Make a reset funciton
 
